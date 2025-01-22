d3f0f196b5d6a01883fd2a16c5c32caced631e35
Lazent <korchagin.oleksii@gmail.com>
2025-01-22 23:19:54 +0100

    Integrated order processing logic in Program.cs

--- OrderProcessingApp/Program.cs
+++ OrderProcessingApp/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using OrderProcessingApp.Factories;
+using OrderProcessingApp.Services;
+
+namespace OrderProcessingApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Order Processing Application");
+
+            IOrderFactory physicalFactory = new PhysicalOrderFactory();
+            IOrderFactory digitalFactory = new DigitalOrderFactory();
+            IOrderFactory subscriptionFactory = new SubscriptionOrderFactory();
+
+            var orderProcessor = new OrderProcessor();
+
+            orderProcessor.AddOrder(physicalFactory.CreateOrder("PH32331"));
+            orderProcessor.AddOrder(digitalFactory.CreateOrder("DG68743"));
+            orderProcessor.AddOrder(subscriptionFactory.CreateOrder("SUB14327"));
+
+            orderProcessor.ProcessAllOrders();
+            Console.WriteLine("\nAll orders processed successfully!");
+        }
+    }
+}
